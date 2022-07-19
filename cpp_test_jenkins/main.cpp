eea6db0527dc10b469b515b9ec01196a708a7642
정등혁 <richard0326@naver.com>
2022-07-19 18:19:28 +0900

    Update main.cpp

--- cpp_test_jenkins/main.cpp
+++ cpp_test_jenkins/main.cpp
@@ -4,18 +4,21 @@ using namespace std;
 
 int main()
 {
-	cout << "Hello Jenkins" << endl;
+	while (1)
+	{
+		cout << "Hello Jenkins" << endl;
 
 #if _WIN64
-	cout << "64 bit environment" << endl;
+		cout << "64 bit environment" << endl;
 #elif _WIN32
-	cout << "32 bit environment" << endl;
+		cout << "32 bit environment" << endl;
 #endif
 
 #if _DEBUG
-	cout << "debug mode" << endl;
+		cout << "debug mode" << endl;
 #elif NDEBUG
-	cout << "release mode" << endl;
+		cout << "release mode" << endl;
 #endif
+	}
 	return 0;
 }
