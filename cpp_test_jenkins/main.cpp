725dcca4882eb84facaccb621ba4d793835c2c6e
정등혁 <richard0326@naver.com>
2022-07-19 14:51:06 +0900

    simple test code

--- cpp_test_jenkins/main.cpp
+++ cpp_test_jenkins/main.cpp
@@ -5,5 +5,17 @@ using namespace std;
 int main()
 {
 	cout << "Hello Jenkins" << endl;
+
+#if _WIN64
+	cout << "64 bit environment" << endl;
+#elif _WIN32
+	cout << "32 bit environment" << endl;
+#endif
+
+#if _DEBUG
+	cout << "debug mode" << endl;
+#elif NDEBUG
+	cout << "release mode" << endl;
+#endif
 	return 0;
 }
